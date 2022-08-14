8dfb53f0b7548a5623e945932109fb45aaa8ccd7
davidyoon <qqpo12@naver.com>
2022-08-14 12:55:26 +0900

    First Commit

---
+++ src/test/java/ycompany/springSample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ycompany.springSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
