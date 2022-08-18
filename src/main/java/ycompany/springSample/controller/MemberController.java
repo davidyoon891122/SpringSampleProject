f23486b5d19c1556a07088cc603fc80addb76222
davidyoon <qqpo12@naver.com>
2022-08-18 19:19:19 +0900

    Add the Join process
    * Added a MemberForm
    * Added a createMemberForm.html

--- src/main/java/ycompany/springSample/controller/MemberController.java
+++ src/main/java/ycompany/springSample/controller/MemberController.java
@@ -2,6 +2,9 @@ package ycompany.springSample.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ycompany.springSample.domain.Member;
 import ycompany.springSample.service.MemberService;
 
 @Controller
@@ -11,5 +14,22 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("form: " + form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
