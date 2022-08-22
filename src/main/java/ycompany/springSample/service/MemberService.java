4a44747a63f39bce3e3c750cb7d915ebb9a41462
davidyoon <qqpo12@naver.com>
2022-08-22 20:19:46 +0900

    Add time calculate logic without AOP
    * Added a System.currentTimeMillies()

--- src/main/java/ycompany/springSample/service/MemberService.java
+++ src/main/java/ycompany/springSample/service/MemberService.java
@@ -22,10 +22,19 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-        validateDuplicatedMember(member); // 중복 회원 검증
+        long start = System.currentTimeMillis();
 
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            validateDuplicatedMember(member); // 중복 회원 검증
+
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
+
     }
 
     private void validateDuplicatedMember(Member member) {
@@ -39,7 +48,15 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
+
     }
 
     public Optional<Member> findOne(Long memberId) {
